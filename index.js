3bfaa0d1663598ff334bf0e94174edeeed45424c
nimamesbah <nimamesbah85@gmail.com>
2026-01-30 22:46:05 +0330

    first step

---
+++ index.js
@@ -0,0 +1,73 @@
+const WebSocket = require("ws");
+const fs = require("fs");
+const path = require("path");
+
+const WS_URL = "wss://mempool.space/api/v1/ws";
+const DATA_FILE = path.join(__dirname, "data.jsonl"); // JSON Lines
+
+let socket;
+let reconnectTimeout = null;
+
+function connect() {
+    console.log("Creating socket...");
+    socket = new WebSocket(WS_URL);
+
+    socket.on("open", () => {
+        console.log("[open] Connection established");
+
+        socket.send(
+            JSON.stringify({
+                action: "want",
+                data: ["blocks", "stats", "mempool-blocks", "live-10h-chart"],
+            })
+        );
+    });
+
+    socket.on("message", (data) => {
+        try {
+            const text = data.toString();
+            const parsed = JSON.parse(text);
+
+            console.log("[message] Data received:", parsed);
+
+
+            fs.appendFile(
+                DATA_FILE,
+                text + "\n",
+                (err) => {
+                    if (err) {
+                        console.error("File append error:", err);
+                    }
+                }
+            );
+        } catch (err) {
+            console.error("Failed to parse message", err);
+        }
+    });
+
+    socket.on("close", (code, reason) => {
+        console.log(
+            `[close] code=${code} reason=${reason.toString()}`
+        );
+
+        scheduleReconnect();
+    });
+
+    socket.on("error", (error) => {
+        console.error("[error] WebSocket error", error);
+        socket.close();
+    });
+}
+
+function scheduleReconnect() {
+    if (reconnectTimeout) return;
+
+    console.log("Reconnecting in 3 seconds...");
+    reconnectTimeout = setTimeout(() => {
+        reconnectTimeout = null;
+        connect();
+    }, 3000);
+}
+
+// start
+connect();
